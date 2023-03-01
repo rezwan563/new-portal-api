274ebe87ec8a9ec139c8bce3faaf5435414ab07b
Rezwan <t.rewan17@gmail.com>
2023-03-01 20:03:14 +0600

    Added category-wise news display functionality

--- js/news.js
+++ js/news.js
@@ -16,7 +16,7 @@ const loadCategories = (categories) => {
     categories.forEach(singleCategory => {
         const category = singleCategory.category_name;
         const news_id = singleCategory.category_id;
-        categoriesContainer.innerHTML += `<p class="hover:underline hover:underline-offset-4 hover:decoration-2 " onclick ="newsInCategory('${news_id}', '${category}')">${category}</p>`
+        categoriesContainer.innerHTML += `<p  class="hover:underline hover:underline-offset-4 hover:decoration-2 " onclick ="newsInCategory('${news_id}', '${category}')">${category}</p>`
 
     })
 }
@@ -27,37 +27,35 @@ const newsInCategory = (category_id, category_name) => {
         .then(res => res.json())
         .then(data => newsDisplay(data.data, category_name))
         .catch(error => console.log(error))
+    newsLoad(category_id)
 }
 
+// Dynamic message for items in a category
 const newsDisplay = (newsList, category) => {
     const newsCountMessage = document.getElementById("news-count-message");
     const newsNumber = newsList.length
     newsCountMessage.innerHTML = `<p class="bg-gray-100 px-8 py-5">${newsNumber} items found in category ${category}</p>`
 }
 
-const newsLoad = () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/08`;
+const newsLoad = (id = "08") => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     fetch(url)
         .then(res => res.json())
         .then(data => allNews(data.data))
         .catch(error => console.log(error))
 }
-
+// Display all news on reload
 const allNews = (data) => {
     console.log(data)
     const newsBox = document.getElementById("news-section");
+    newsBox.innerHTML = ""
     data.forEach(singleStory => {
         const { author, image_url, title, details } = singleStory;
         // console.log(author)
         const { name, img, published_date } = author;
-        console.log(img)
-        console.log(name)
-        console.log(published_date)
-
-        console.log()
         newsBox.innerHTML += `
-        <div class="card lg:card-side bg-base-100 shadow-xl mb-5 ">
-              <figure><img src="${image_url}" class="h-36" alt="Album"/></figure>
+        <div class="card lg:card-side bg-base-100 shadow-xl mb-5 h-46">
+              <figure><img src="${image_url}" class="h-46 pl-4" alt="Album"/></figure>
               <div class="card-body">
                 <h2 class="card-title">${title}</h2>
                 <div>
